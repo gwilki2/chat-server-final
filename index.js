5d06e7d26bb122fb255e52657f41269703994942
George Wilkins <gwilki2@gmail.com>
2022-03-22 08:40:31 -0500

    completed most basic functionalities

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const app = require('./src/app')
 const port = require('./src/config/appConfig').port || 3002
+const http = require('http')
+const SocketServer = require('./src/socket')
 
-app.listen(port, () => console.log(`app listening on port ${port}`))
+const server = http.createServer(app)
+
+SocketServer(server)
+
+
+server.listen(port, () => console.log(`app listening on port ${port}`))
---
+++ src/socket/index.js
@@ -0,0 +1,21 @@
+const socketIo = require('socket.io')
+
+const SocketServer = (server) => {
+    const io = socketIo(server, {
+        cors: {
+            origin: "*"
+        }
+    })
+
+    io.on('connection', (socket) => {
+        socket.on('join', async (user) => {
+            console.log("New user joined: ", user.firstName)
+        })
+
+        socket.on('message', async (message) => {
+            io.emit('message', message)
+        })
+    })
+}
+
+module.exports = SocketServer
