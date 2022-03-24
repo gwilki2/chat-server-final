f320a919ad318ffb8e9e4f3ce50e4062696442fc
George Wilkins <gwilki2@gmail.com>
2022-03-24 05:35:30 -0500

    added test route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const authRoutes = require('./auth')
 
+
+router.get('/test', (req, res) => {
+    res.send(`I'm Online`)
+})
 router.use(authRoutes)
 
 module.exports = router
