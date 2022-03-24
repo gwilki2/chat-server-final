2fd17f2a83735e1868ed434c43b93be6d4b0e56a
George Wilkins <gwilki2@gmail.com>
2022-03-24 05:50:11 -0500

    moved test response to root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const authRoutes = require('./auth')
 
 
-router.get('/test', (req, res) => {
+router.get('/', (req, res) => {
     res.send(`I'm Online`)
 })
 router.use(authRoutes)
