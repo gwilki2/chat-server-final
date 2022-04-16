9ce700a725e5f4f2e8598d2af7d47cbe803832a0
George Wilkins <gwilki2@gmail.com>
2022-04-15 21:11:24 -0500

    added check on update to see if password notEmpty before validating

--- src/validate/auth/updateRules.js
+++ src/validate/auth/updateRules.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator')
 
 module.exports = [
     body('email').isEmail(),
-    body('password').isLength({ min: 6 }),
+    body('password').if(body('password').notEmpty()).isLength({ min: 6 }),
     body('firstName').notEmpty(), 
     body('lastName').notEmpty(),
     body('lang').notEmpty(),
